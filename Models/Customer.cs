03546ef467c1bf85504bc68b4635d332f84eb3c2
Dimitris Grevenos <dim.grev@outlook.com>
2020-05-02 11:39:21 +0300

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelCrm.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string lastname { get; set; }
+		public string address { get; set; }
+		public string email { get; set; }
+		public string vatnumber { get; set; }
+		public string phone { get; set; }
+		public decimal TotalGross { get; set; }
+		public bool IsActive { get; set; }
+		public DateTime Dob { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public List<Order> Orders { get; set; }
+
+	}
+}
